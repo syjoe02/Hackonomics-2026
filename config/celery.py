dcbaa7673e93a9fc59cee864db11d6250de54730
syjoe <csy5501@gmail.com>
2026-01-20 17:07:24 +0900

    Add: celery

--- config/celery.py
+++ config/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-app = Celery("config")
+app = Celery("hackonomics")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
