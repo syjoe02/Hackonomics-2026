7082a7e6e4974e69f6e76d8ecce24a9591932001
syjoe <csy5501@gmail.com>
2026-02-24 15:38:44 +0900

    Edit: signup events

--- events/application/publishers/domain_event_publisher.py
+++ events/application/publishers/domain_event_publisher.py
@@ -5,11 +5,5 @@ class DomainEventPublisher:
     def __init__(self, repository):
         self.repository = repository
 
-    def publish(self, *, aggregate_type, aggregate_id, event_type, payload):
-        event = DomainEvent(
-            aggregate_type=aggregate_type,
-            aggregate_id=str(aggregate_id),
-            event_type=event_type,
-            payload=payload,
-        )
+    def publish(self, event: DomainEvent) -> None:
         self.repository.save(event)
--- events/application/publishers/event_dispatcher.py
+++ events/application/publishers/event_dispatcher.py
@@ -2,21 +2,12 @@ from events.application.publishers.domain_event_publisher import DomainEventPubl
 from events.application.publishers.handlers.user_event_publisher import (
     UserEventPublisher,
 )
-from events.domain.entities import DomainEvent
 
 
 class EventDispatcher:
     def __init__(self, repository):
         self.event_publisher = DomainEventPublisher(repository)
-        self.user_publisher = UserEventPublisher()
 
     def publish_user_signup(self, user):
-        data = self.user_publisher.user_signup(user)
-
-        event = DomainEvent(
-            aggregate_type=data["aggregate_type"],
-            aggregate_id=str(data["aggregate_id"]),
-            event_type=data["event_type"],
-            payload=data["payload"],
-        )
-        self.repository.save(event)
+        event = UserEventPublisher.user_signed_up(user)
+        self.event_publisher.publish(event)
